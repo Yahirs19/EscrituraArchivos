2b0e5309f167bc0b64b24ea44d1c267cd5ae8f84
Yahirs19 <L21212055@tectijuana.edu.mx>
2022-05-31 11:24:09 -0700

    Update1

--- EscrituraArchivos/Program.cs
+++ EscrituraArchivos/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
+
 
 namespace EscrituraArchivos
 {
@@ -10,7 +13,28 @@ namespace EscrituraArchivos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            StreamWriter sw = new StreamWriter("ejemplo.txt", true);
+            // Si el archivo no existe lo creara
+            // Si ya existe, escribira en el 
+            // true es para agregar y no para sobre escribir 
+            string[] linea =
+            {
+                "Esta es la informacion de la primera linea",
+                "Esta es la segunda linea ",
+                "Fin del texto"
+            };
+            foreach (string line in linea)
+            {
+                sw.WriteLine(line);
+            }
+            sw.Close();// Se debe cerrar el archivo
+
+            Console.WriteLine("Escribiendo en el archivo. . .");
+            Console.ReadLine();
+             for (int i = 0; i < linea.Length; i++) 
+             {
+            Console.WriteLine(linea[i]);
+           }
         }
     }
 }
